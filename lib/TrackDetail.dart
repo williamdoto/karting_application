cc89a88d9cc24dd262ef8600fb434c335e279817
williamdoto <87979482+williamdoto@users.noreply.github.com>
2023-07-16 18:52:09 +1000

    Added Ratings, Reviews, and Cards to view those respective things

--- lib/TrackDetail.dart
+++ lib/TrackDetail.dart
@@ -49,13 +49,28 @@ class _TrackDetailState extends State<TrackDetail> {
     MapsLauncher.launchQuery(widget.kartPlace['name']);
   }
 
+  Future<void> _launchCall(String? phoneNumber) async {
+    if (phoneNumber == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Phone Number isn't available")));
+    } else {
+      String sanitizedNumber = phoneNumber.replaceAll(RegExp(r'[^\d+]'), '');
+      Uri url = Uri.parse('tel:$sanitizedNumber');
+      if (await canLaunchUrl(url)) {
+        await launchUrl(url);
+      } else {
+        throw 'Could not launch $url';
+      }
+    }
+  }
+
   Future<void> fetchPlaceDetails(String placeId) async {
     String apiKey =
         'AIzaSyDINb5jEJSNl4aLsbGCBXSiImHMTnajoGw'; // Enter your Google API Key
 
     final response = await http.get(
       Uri.parse(
-        'https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&fields=formatted_phone_number,website&key=$apiKey',
+        'https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&fields=formatted_phone_number,website,rating,opening_hours,reviews&reviews_sort=most_relevant&key=$apiKey',
       ),
     );
 
@@ -102,9 +117,23 @@ class _TrackDetailState extends State<TrackDetail> {
 
   @override
   Widget build(BuildContext context) {
-    String? phoneNumber =
+    late String? phoneNumber =
         placeDetails != null ? placeDetails!['formatted_phone_number'] : 'N/A';
-    String? website = placeDetails != null ? placeDetails!['website'] : 'N/A';
+    late String? website =
+        placeDetails != null ? placeDetails!['website'] : 'N/A';
+    //     String? openingHours = placeDetails != null
+    //     ? (placeDetails!['opening_hours']['open_now'] ? 'Open Now' : 'Closed')
+    //     : 'N/A';
+    // double? rating = placeDetails != null ? placeDetails!['rating'] : 'N/A';
+    // String? priceLevel = placeDetails != null ? placeDetails!['price_level'].toString() : 'N/A';
+    late double? rating = placeDetails != null ? placeDetails!['rating'] : 0.0;
+    // Fetch opening hours
+    List<dynamic>? openingHours = placeDetails != null
+        ? (placeDetails!['opening_hours']?['weekday_text'] as List<dynamic>?)
+        : [];
+    List<dynamic>? reviews = placeDetails != null
+        ? placeDetails!['reviews'] as List<dynamic>?
+        : null;
 
     String apiKey =
         'AIzaSyDINb5jEJSNl4aLsbGCBXSiImHMTnajoGw'; // Enter your Google API Key
@@ -114,91 +143,181 @@ class _TrackDetailState extends State<TrackDetail> {
         .toList();
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.kartPlace['name']),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.favorite,
-                color: isTrackAlreadySaved ? Colors.red : Colors.grey),
-            onPressed: saveTrack,
-          ),
-        ],
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          // Image Carousel
-          CarouselSlider(
-            options: CarouselOptions(height: 200.0),
-            items: photos.map((photoUrl) {
-              return Builder(
-                builder: (BuildContext context) {
-                  return Image.network(
-                    photoUrl,
-                    fit: BoxFit.cover,
+        appBar: AppBar(
+          title: Text(widget.kartPlace['name']),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.favorite,
+                  color: isTrackAlreadySaved ? Colors.red : Colors.grey),
+              onPressed: saveTrack,
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+            child: ConstrainedBox(
+          constraints: BoxConstraints(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              // Image Carousel
+              CarouselSlider(
+                options: CarouselOptions(height: 200.0),
+                items: photos.map((photoUrl) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Image.network(
+                        photoUrl,
+                        fit: BoxFit.cover,
+                      );
+                    },
                   );
-                },
-              );
-            }).toList(),
-          ),
-          const SizedBox(height: 20),
-          // Buttons
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                ElevatedButton(
-                  child: const Text('Directions'),
-                  onPressed: () => _launchURL(),
+                }).toList(),
+              ),
+              const SizedBox(height: 20),
+              // Buttons
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    ElevatedButton(
+                      child: const Text('Directions'),
+                      onPressed: () => _launchURL(),
+                    ),
+                    ElevatedButton(
+                      child: const Text('Record'),
+                      onPressed: () {},
+                    ),
+                    ElevatedButton(
+                      onPressed: () => _launchCall(phoneNumber),
+                      child: const Text('Call'),
+                    ),
+                  ],
                 ),
-                ElevatedButton(
-                  child: const Text('Record'),
-                  onPressed: () {},
-                ),
-                ElevatedButton(
-                  onPressed: saveTrack,
-                  child: const Text('Call'),
-                ),
-              ],
-            ),
-          ),
-          // Track Details
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  'Track Detail',
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-                const SizedBox(height: 20),
-                Text(
-                  'Name: ${widget.kartPlace['name']}',
-                  style: Theme.of(context).textTheme.bodyText1,
-                ),
-                Text(
-                  'Address: ${widget.kartPlace['vicinity']}',
-                  style: Theme.of(context).textTheme.bodyText1,
-                ),
-                Text(
-                  'Phone: $phoneNumber',
-                  style: Theme.of(context).textTheme.bodyText1,
-                ),
-                Text(
-                  'Website: $website',
-                  style: Theme.of(context).textTheme.bodyText1,
-                ),
-                // ...you can add more details here
-              ],
-            ),
-          ),
+              ),
+              // Track Details
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Track Detail',
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      'Name: ${widget.kartPlace['name']}',
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    Text(
+                      'Address: ${widget.kartPlace['vicinity']}',
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    Text(
+                      'Phone: $phoneNumber',
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    Text(
+                      'Website: $website',
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    Text(
+                      'Rating: $rating',
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    if (openingHours != null) ...[
+                      Text(
+                        'Opening Hours:',
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                      for (var hours in openingHours)
+                        Text(
+                          hours,
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                    ],
+                    if (reviews != null) ...[
+                      const SizedBox(height: 20),
+                      Text(
+                        'Reviews:',
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                      SizedBox(
+                        height: 200,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: reviews.length,
+                          itemBuilder: (context, index) {
+                            var review = reviews[index];
+                            var reviewText = review['text'];
+                            var truncatedReviewText = reviewText.length > 100
+                                ? reviewText.substring(0, 100) + '...'
+                                : reviewText;
 
-          const SizedBox(height: 20)
-        ],
-      ),
-    );
+                            return InkWell(
+                              onTap: reviewText.length > 100
+                                  ? () {
+                                      showModalBottomSheet(
+                                        context: context,
+                                        builder: (context) {
+                                          return Container(
+                                            padding: EdgeInsets.all(10.0),
+                                            child: Text(reviewText),
+                                          );
+                                        },
+                                      );
+                                    }
+                                  : null,
+                              child: SizedBox(
+                                width: 300, // set the width of the review card
+                                child: Card(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(10.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          'Reviewer: ${review['author_name']}',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .titleLarge,
+                                        ),
+                                        Text(
+                                          'Rating: ${review['rating']}',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .titleMedium,
+                                        ),
+                                        Text(
+                                          'Review: $truncatedReviewText',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodyMedium,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ]
+
+                    // ...you can add more details here
+
+                    // ...you can add more details here
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 20)
+            ],
+          ),
+        )));
   }
 }
