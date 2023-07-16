2cb8d4743c165fb60402c3de6a75ebda8499e715
Hiu Lam Lau <hlau0019@student.monash.edu>
2023-07-17 01:50:53 +1000

    update record widget

--- lib/RecordCard.dart
+++ lib/RecordCard.dart
@@ -1,32 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:karting_application/RecordPage.dart';
 import 'package:karting_application/models/record_model.dart';
 
-
-class RecordCard extends StatelessWidget {
+class RecordCard extends StatefulWidget {
   final Record record;
 
   RecordCard({required this.record});
 
   @override
+  _RecordCardState createState() => _RecordCardState();
+}
+
+class _RecordCardState extends State<RecordCard> {
+  @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(
-        leading: Icon(Icons.album),
-        title: Text(record.trackName),
-        subtitle: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Date: ${record.recordDate}'),
-            Text('Lap: ${record.recordLap}'),
-            Text('Avg Time: ${record.recordAvgTime}'),
-            Text('Fastest Lap: ${record.recordFastestLap}'),
-            Text('Position: ${record.recordPos}'),
-          ],
+      child: ExpansionTile(
+        leading: Icon(Icons.sports_score),
+        title: Text(
+          widget.record.trackName,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18.0,
+          ),
         ),
+        subtitle: Text(
+          'Date: ${DateFormat('yyyy-MM-dd').format(widget.record.recordDate)}',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16.0,
+          ),
+        ),
+        children: <Widget>[
+          ListTile(
+            title: Text('Lap: ${widget.record.recordLap}'),
+          ),
+          ListTile(
+            title: Text('Avg Time: ${widget.record.recordAvgTime}'),
+          ),
+          ListTile(
+            title: Text('Fastest Lap: ${widget.record.recordFastestLap}'),
+          ),
+          ListTile(
+            title: Text('Position: ${widget.record.recordPos}'),
+          ),
+        ],
       ),
     );
   }
 }
-
-
